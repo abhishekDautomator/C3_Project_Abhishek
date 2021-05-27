676ed41fd78b421f7a4795c3099de5098776539b
Abhishek Narayan <narayan.abhishek.28@gmail.com>
2021-05-27 23:10:24 +0530

    Part 3: Failing test case

--- src/test/java/RestaurantTest.java
+++ src/test/java/RestaurantTest.java
@@ -3,6 +3,8 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -67,4 +69,52 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    @Test
+    public void total_cost_for_items_added_by_the_user_should_equal_to_sum_of_the_item_cost_in_menu() throws itemNotFoundException {
+        restaurant = new Restaurant("Restro1","Patna",LocalTime.of(10,0),LocalTime.of(18,0));
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Roll",100);
+        List<String> i = new ArrayList<>();
+        i.add("Sweet corn soup");
+        i.add("Vegetable lasagne");
+        assertEquals(restaurant.getTotalCost(i),269+119);
+    }
+
+    @Test
+    public void total_cost_for_items_added_by_the_user_should_not_equal_to_sum_of_the_other_item_cost_in_menu() throws itemNotFoundException {
+        restaurant = new Restaurant("Restro1","Patna",LocalTime.of(10,0),LocalTime.of(18,0));
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Roll",100);
+        List<String> i = new ArrayList<>();
+        i.add("Sweet corn soup");
+        i.add("Vegetable lasagne");
+        assertNotEquals(restaurant.getTotalCost(i),269+100);
+    }
+
+    @Test
+    public void total_cost_should_be_0_when_no_item_is_added() throws itemNotFoundException {
+        restaurant = new Restaurant("Restro1","Patna",LocalTime.of(10,0),LocalTime.of(18,0));
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Roll",100);
+        List<String> i = new ArrayList<>();
+        assertEquals(restaurant.getTotalCost(i),0);
+    }
+
+    @Test
+    public void item_not_found_exception_should_be_thrown_if_user_add_an_item_out_of_the_menu_to_calculate_the_total_cost() throws itemNotFoundException {
+        restaurant = new Restaurant("Restro1","Patna",LocalTime.of(10,0),LocalTime.of(18,0));
+        restaurant.addToMenu("Vegetable lasagne", 269);
+        restaurant.addToMenu("Sweet corn soup",119);
+        restaurant.addToMenu("Roll",100);
+        List<String> i = new ArrayList<>();
+        i.add("Sweet corn soup");
+        i.add("Pasta");
+        assertThrows(itemNotFoundException.class,()->{
+            restaurant.getTotalCost(i);
+        });
+    }
 }
